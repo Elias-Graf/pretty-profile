bbb6bd6bd06c1fdfdac82f4bb952abda452516a3
Elias Graf <p.elias.graf@gmail.com>
2022-07-11 17:51:47 +0200

    Fixed dev mode At least for linux. I don't think this works work windows (env var).

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-const dev = process.env.NODE_ENV === 'development';
+const dev = process.env['NODE_ENV'];
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
 	kit: {
 		adapter: adapter(),
@@ -16,9 +14,6 @@ const config = {
 		paths: {
 			base: dev ? "" : "/pretty-profile",
 		},
-		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-		// For example, instead of '_app', use 'app_', 'internal', etc.
-		appDir: 'internal',
 	}
 };
 
